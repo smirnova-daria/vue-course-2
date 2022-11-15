07c48728176685779cb7d2962ffb08ae75b051ea
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-15 21:40:52 +0300

    add friend contact component

---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<template>
+  <section>
+    <header>
+      <h1>My friends</h1>
+    </header>
+    <ul>
+      <friend-contact
+        name="Liza Mironova"
+        phone-number="0123 4567 8999"
+        email-address="liza@hello.js"
+        :is-favorite="true"
+      ></friend-contact>
+      <friend-contact
+        name="Masha Chervyakova"
+        phone-number="0123 4567 8999"
+        email-address="masha@hello.js"
+      ></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "liza",
+          name: "Liza Mironova",
+          phone: "0123 4567 890",
+          email: "liza@hello.js",
+        },
+        {
+          id: "masha",
+          name: "Masha Chervyakova",
+          phone: "0123 4567 890",
+          email: "masha@hello.vue",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,69 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFriendFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleDetails">Show details</button>
+    <button @click="toggleFavorite">
+      {{ isFriendFavorite ? "Remove from " : "Add to " }} favorite
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong> {{ phoneNumber }}</li>
+      <li><strong>Email: </strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function (value) {
+      //   return value === "1" || value === "0";
+      // },
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      isFriendFavorite: this.isFavorite,
+      friend: {
+        id: "liza",
+        name: "Liza Mironova",
+        phone: "0123 4567 890",
+        email: "liza@hello.js",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      // if (this.isFriendFavorite === "1") {
+      //   this.isFriendFavorite = "0";
+      // } else {
+      //   this.isFriendFavorite = "1";
+      // }
+
+      this.isFriendFavorite = !this.isFriendFavorite;
+    },
+  },
+};
+</script>
+
+<style></style>
