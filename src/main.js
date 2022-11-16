e9a459299b3149865d4ebd6ea16762256beaef91
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-16 21:39:22 +0300

    init new project

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import BaseCard from "./components/UI/BaseCard.vue";
-import BaseButton from "./components/UI/BaseButton.vue";
-import BaseDialog from "./components/UI/BaseDialog.vue";
+import { createApp } from 'vue';
 
-const app = createApp(App);
-app.component("base-card", BaseCard);
-app.component("base-button", BaseButton);
-app.component("base-dialog", BaseDialog);
+import App from './App.vue';
 
-app.mount("#app");
+createApp(App).mount('#app');
