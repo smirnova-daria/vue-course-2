ee356417014693dcf194f021d32ea380fe5dcaf6
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-16 21:23:29 +0300

    add dialog for display error

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,4 +1,16 @@
 <template>
+  <base-dialog
+    v-if="inputIsInvalid"
+    title="Invalid Input"
+    @close="confirmError"
+  >
+    <template #default>
+      <p>At least one input value is invalid</p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Okay</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -26,14 +38,34 @@
 </template>
 
 <script>
+import BaseButton from "../UI/BaseButton.vue";
 export default {
+  components: { BaseButton },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
   inject: ["addResource"],
   methods: {
     submitData() {
       const enteredTitle = this.$refs.titleInput.value;
       const enteredDescription = this.$refs.descInput.value;
       const enteredUrl = this.$refs.linkInput.value;
+
+      if (
+        enteredTitle.trim() === "" ||
+        enteredDescription.trim() === "" ||
+        enteredUrl.trim() === ""
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+
       this.addResource(enteredTitle, enteredDescription, enteredUrl);
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
     },
   },
 };
