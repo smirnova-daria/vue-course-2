4c9d09a5447b8b4dc17d7a5e276bac14ae74ea0a
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-16 21:03:14 +0300

    add feature to add new resource

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,9 +1,68 @@
 <template>
-  <h2>Add Resource</h2>
+  <base-card>
+    <form @submit.prevent="submitData">
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input id="title" type="text" name="title" ref="titleInput" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea
+          name="description"
+          id="description"
+          rows="3"
+          ref="descInput"
+        ></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input id="link" type="url" name="link" ref="linkInput" />
+      </div>
+      <div>
+        <base-button type="submit">Add resource</base-button>
+      </div>
+    </form>
+  </base-card>
 </template>
 
 <script>
-export default {};
+export default {
+  inject: ["addResource"],
+  methods: {
+    submitData() {
+      const enteredTitle = this.$refs.titleInput.value;
+      const enteredDescription = this.$refs.descInput.value;
+      const enteredUrl = this.$refs.linkInput.value;
+      this.addResource(enteredTitle, enteredDescription, enteredUrl);
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -11,7 +11,9 @@
       >Add Resource</base-button
     >
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -42,12 +44,24 @@ export default {
   provide() {
     return {
       resources: this.storedResources,
+      addResource: this.addResource,
     };
   },
 
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+
+    addResource(title, description, link) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title,
+        description,
+        link,
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = "stored-resources";
     },
   },
 
