1d465340cefe90bfbb3daf35826f2c90838eb139
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-16 15:04:24 +0300

    add work with slots

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,10 +1,13 @@
 <template>
   <section>
-    <header>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </header>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <template v-slot:header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+
+      <p>{{ infoText }}</p>
+    </base-card>
   </section>
 </template>
 
@@ -13,19 +16,3 @@ export default {
   props: ["fullName", "infoText", "role"],
 };
 </script>
-
-<style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
-section header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-</style>
