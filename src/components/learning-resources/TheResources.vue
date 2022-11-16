fe063d16a84578529839e303e3375262cfd38678
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-16 21:33:58 +0300

    add feature to delete resource

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -45,6 +45,7 @@ export default {
     return {
       resources: this.storedResources,
       addResource: this.addResource,
+      removeResource: this.removeResource,
     };
   },
 
@@ -62,6 +63,11 @@ export default {
       };
       this.storedResources.unshift(newResource);
       this.selectedTab = "stored-resources";
+    },
+
+    removeResource(id) {
+      const resIndex = this.storedResources.findIndex((res) => res.id === id);
+      this.storedResources.splice(resIndex, 1);
     },
   },
 
