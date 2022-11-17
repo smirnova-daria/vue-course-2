c887718276182bb44ed283ad6d71b4373cb81d64
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-17 19:29:29 +0300

    add work with fetching and sending data

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,9 +3,16 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">
+        No stored experiences found.
+      </p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -18,12 +25,48 @@
 </template>
 
 <script>
-import SurveyResult from './SurveyResult.vue';
+import SurveyResult from "./SurveyResult.vue";
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null,
+    };
+  },
+  mounted() {
+    this.loadExperiences();
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      this.error = null;
+      fetch(
+        "https://vue-http-demo-574f5-default-rtdb.firebaseio.com/surveys.json"
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const results = [];
+          for (const id in data) {
+            results.push({ id, name: data[id].name, rating: data[id].rating });
+          }
+          this.results = results;
+        })
+        .catch((error) => {
+          this.isLoading = false;
+          this.error = "Failed to fetch data - please try again";
+          console.error(error);
+        });
+    },
   },
 };
 </script>
