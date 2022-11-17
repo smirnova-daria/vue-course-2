514ea3f4df3f6e7c1eaf2573efd765d3a1d4bb12
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-17 21:30:18 +0300

    add work with vue-router

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,17 +1,28 @@
 <template>
+  <button @click="confirmInput">Confirm</button>
   <ul>
-    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    <user-item
+      v-for="user in users"
+      :key="user.id"
+      :name="user.fullName"
+      :role="user.role"
+    ></user-item>
   </ul>
 </template>
 
 <script>
-import UserItem from './UserItem.vue';
+import UserItem from "./UserItem.vue";
 
 export default {
   components: {
     UserItem,
   },
-  inject: ['users'],
+  inject: ["users"],
+  methods: {
+    confirmInput() {
+      this.$router.push("/teams");
+    },
+  },
 };
 </script>
 
