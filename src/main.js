51af330fd4e72d15befa61a33eff8e332faa84ba
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-18 15:19:21 +0300

    add rendering multiple routes

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import TeamsList from "./components/teams/TeamsList.vue";
 import UsersList from "./components/users/UsersList.vue";
 import TeamMembers from "./components/teams/TeamMembers.vue";
 import NotFound from "./components/nav/NotFound.vue";
+import TeamsFooter from "./components/teams/TeamsFooter.vue";
+import UsersFooter from "./components/users/UsersFooter.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,7 +15,7 @@ const router = createRouter({
     {
       path: "/teams",
       name: "teams",
-      component: TeamsList,
+      components: { default: TeamsList, footer: TeamsFooter },
       // alias: "/",
       children: [
         {
@@ -24,7 +26,7 @@ const router = createRouter({
         },
       ],
     },
-    { path: "/users", component: UsersList },
+    { path: "/users", components: { default: UsersList, footer: UsersFooter } },
     { path: "/:notFound(.*)", component: NotFound },
   ],
 });
