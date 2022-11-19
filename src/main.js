5cca91882a523a2f5bd6eababb4be772d58fa225
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-19 15:36:31 +0300

    register new store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
+import { createStore } from "vuex";
+import App from "./App.vue";
 
-import App from './App.vue';
+const store = createStore({
+  state() {
+    return { counter: 0 };
+  },
+});
 
 const app = createApp(App);
 
-app.mount('#app');
+app.use(store);
+
+app.mount("#app");
