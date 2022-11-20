81519e0ce77e2d702f997908e75f1676bcda9a05
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-20 15:28:21 +0300

    add registering main components

--- src/router.js
+++ src/router.js
@@ -1,18 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
+import CoachDetails from "./pages/coaches/CoachDetails.vue";
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import CoachRegister from "./pages/coaches/CoachRegister.vue";
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import RequestReceived from "./pages/requests/RequestReceived.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/coaches" },
-    { path: "/coaches", component: null },
+    { path: "/coaches", component: CoachesList },
     {
       path: "/coaches/:id",
-      component: null,
-      children: [{ path: "contact", component: null }],
+      component: CoachDetails,
+      children: [{ path: "contact", component: ContactCoach }],
     },
-    { path: "/register", component: null },
-    { path: "/requests", component: null },
-    { path: "/:notFound(.*)", component: null },
+    { path: "/register", component: CoachRegister },
+    { path: "/requests", component: RequestReceived },
+    { path: "/:notFound(.*)", component: NotFound },
   ],
 });
 
