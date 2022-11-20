00c76a210defcc7545944baef836f7b43d54024d
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-20 16:25:44 +0300

    add layout of coaches list

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,16 +6,24 @@
       <router-link to="/register">Register as coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
     </ul>
     <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from "@/components/coaches/CoachItem.vue";
 export default {
+  components: { CoachItem },
   computed: {
     filteredCoaches() {
       return this.$store.getters["coaches/coaches"];
@@ -27,4 +35,15 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
