04652b7ff24f592f331537c9a307f04f4caf8c74
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-21 19:50:29 +0300

    add output incoming requests

--- src/store/modules/requests/index.js
+++ src/store/modules/requests/index.js
@@ -1,6 +1,6 @@
-import actions from "./actions";
-import getters from "./getters";
-import mutations from "./mutations";
+import actions from "./actions.js";
+import getters from "./getters.js";
+import mutations from "./mutations.js";
 
 export default {
   namespaced: true,
