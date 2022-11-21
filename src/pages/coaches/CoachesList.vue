4b54e361d7520ee1c6fcbf91a4603bbb0aeaac03
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-21 20:43:41 +0300

    add fetching data about coaches from firebase

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,7 +5,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
         <base-button v-if="!isCoach" link to="/register"
           >Register as coach</base-button
         >
@@ -67,6 +67,13 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
+    loadCoaches() {
+      this.$store.dispatch("coaches/loadCoaches");
+    },
+  },
+
+  created() {
+    this.loadCoaches();
   },
 };
 </script>
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -26,4 +26,32 @@ export default {
       id: userId,
     });
   },
+
+  async loadCoaches(context) {
+    const res = await fetch(
+      `https://vue-find-coach-d856f-default-rtdb.firebaseio.com/coaches.json`
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      //
+    }
+
+    const coaches = [];
+
+    for (const key in data) {
+      const coach = {
+        id: key,
+        firstName: data[key].firstName,
+        lastName: data[key].lastName,
+        description: data[key].description,
+        hourlyRate: data[key].hourlyRate,
+        areas: data[key].areas,
+      };
+      coaches.push(coach)
+    }
+
+    context.commit('setCoaches', coaches)
+  },
 };
