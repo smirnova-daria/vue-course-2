05c2e89180164279174f0d0698b7318e049856f1
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-21 22:09:04 +0300

    add not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,4 +1,14 @@
-<template>NOT FOUND</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
 
 <script>
 export default {};
