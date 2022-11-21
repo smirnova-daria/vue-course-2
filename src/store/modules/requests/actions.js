ea76a5e56dc3788acb2b471450fe5173212a821c
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-21 21:42:57 +0300

    add sending coaching requests

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,53 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+    const res = await fetch(
+      `https://vue-find-coach-d856f-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: "POST",
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      const error = new Error(data.message || "Failed to send request");
+      throw error;
+    }
+
+    newRequest.id = data.name;
+    newRequest.coachId = payload.coachId;
+
     context.commit("addRequest", newRequest);
+  },
+
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const res = await fetch(
+      `https://vue-find-coach-d856f-default-rtdb.firebaseio.com/requests/${coachId}.json`
+    );
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(data.message || "Failed to fetch requests");
+    }
+
+    const requests = [];
+
+    for (const key in data) {
+      const request = {
+        id: key,
+        coachId,
+        userEmail: data[key].userEmail,
+        message: data[key].message,
+      };
+      requests.push(request);
+    }
+
+    context.commit("setRequests", requests);
   },
 };
