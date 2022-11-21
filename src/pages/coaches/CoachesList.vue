be06ab9494cb76f5a44ef4b1064bc2017a8b10de
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-21 15:26:18 +0300

    add coaches to state

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,7 +6,9 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline">Refresh</base-button>
-        <base-button link to="/register">Register as coach</base-button>
+        <base-button v-if="!isCoach" link to="/register"
+          >Register as coach</base-button
+        >
       </div>
       <ul v-if="hasCoaches">
         <coach-item
@@ -39,6 +41,9 @@ export default {
     };
   },
   computed: {
+    isCoach() {
+      return this.$store.getters["coaches/isCoach"];
+    },
     filteredCoaches() {
       const coaches = this.$store.getters["coaches/coaches"];
       return coaches.filter((coach) => {
