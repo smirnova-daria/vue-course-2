cc8945f51877af9a6a0811ead453cd6d810cf059
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-22 15:32:03 +0300

    add login action

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,33 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const res = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCTdYpxV8DotU_uzPWM2PgUQx9hUGcZJWg`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      console.log(data);
+      throw new Error(
+        data.message || "Failed to authenticate. Check your login data."
+      );
+    }
+
+    console.log(data);
+    context.commit("setUser", {
+      token: data.idToken,
+      userId: data.localId,
+      tokenExpiration: data.expiresIn,
+    });
+  },
   async signup(context, payload) {
     const res = await fetch(
       `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCTdYpxV8DotU_uzPWM2PgUQx9hUGcZJWg`,
