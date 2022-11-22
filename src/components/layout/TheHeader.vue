cf63de960a9002e57bd62f44301fceaab9194f29
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-22 16:13:06 +0300

    add logout functionality

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -8,6 +8,9 @@
           <router-link to="/requests">Requests</router-link>
         </li>
         <li v-else><router-link to="/auth">Login</router-link></li>
+        <li v-if="isLoggedIn">
+          <base-button @click="logout">Logout</base-button>
+        </li>
       </ul>
     </nav>
   </header>
@@ -18,6 +21,11 @@ export default {
   computed: {
     isLoggedIn() {
       return this.$store.getters.isAuthenticated;
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch("logout");
     },
   },
 };
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -57,4 +57,11 @@ export default {
       tokenExpiration: data.expiresIn,
     });
   },
+  logout(context) {
+    context.commit("setUser", {
+      token: null,
+      userId: null,
+      tokenExpiration: null,
+    });
+  },
 };
