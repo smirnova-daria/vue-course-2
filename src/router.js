43b9fe71019170f2572e504761a05f2ef1993986
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-22 19:23:29 +0300

    add lazy loading of components

--- src/router.js
+++ src/router.js
@@ -1,12 +1,24 @@
+import { defineAsyncComponent } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
-import CoachDetails from "./pages/coaches/CoachDetails.vue";
 import CoachesList from "./pages/coaches/CoachesList.vue";
-import CoachRegister from "./pages/coaches/CoachRegister.vue";
-import ContactCoach from "./pages/requests/ContactCoach.vue";
-import RequestReceived from "./pages/requests/RequestReceived.vue";
 import NotFound from "./pages/NotFound.vue";
-import UserAuth from "./pages/auth/UserAuth.vue";
 import store from "./store/index.js";
+
+const CoachDetails = defineAsyncComponent(() =>
+  import("./pages/coaches/CoachDetails.vue")
+);
+const CoachRegister = defineAsyncComponent(() =>
+  import("./pages/coaches/CoachRegister.vue")
+);
+const ContactCoach = defineAsyncComponent(() =>
+  import("./pages/requests/ContactCoach.vue")
+);
+const RequestReceived = defineAsyncComponent(() =>
+  import("./pages/requests/RequestReceived.vue")
+);
+const UserAuth = defineAsyncComponent(() =>
+  import("./pages/auth/UserAuth.vue")
+);
 
 const router = createRouter({
   history: createWebHistory(),
