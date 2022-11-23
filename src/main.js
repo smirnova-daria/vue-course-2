ae79ba6cfa356c66adea222493ed068b599e126d
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-23 13:51:51 +0300

    init new project

--- src/main.js
+++ src/main.js
@@ -1,25 +1,5 @@
-import { createApp, defineAsyncComponent } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store/index";
-import BaseCard from "./components/ui/BaseCard.vue";
-import BaseButton from "./components/ui/BaseButton.vue";
-import BaseBadge from "./components/ui/BaseBadge.vue";
-import BaseSpinner from "./components/ui/BaseSpinner.vue";
-// import BaseDialog from "./components/ui/BaseDialog.vue";
+import { createApp } from 'vue';
 
-const BaseDialog = defineAsyncComponent(() =>
-  import("./components/ui/BaseDialog.vue")
-);
-const app = createApp(App);
+import App from './App.vue';
 
-app.use(router);
-app.use(store);
-
-app.component("base-card", BaseCard);
-app.component("base-button", BaseButton);
-app.component("base-badge", BaseBadge);
-app.component("base-spinner", BaseSpinner);
-app.component("base-dialog", BaseDialog);
-
-app.mount("#app");
+createApp(App).mount('#app');
