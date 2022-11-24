c0db159aae99e59cea4b95bdee3f35e618945c74
Daria Smirnova <darya.smirn1@yandex.ru>
2022-11-24 16:40:22 +0300

    add work with custom hooks

---
+++ src/components/users/UserList.vue
@@ -0,0 +1,70 @@
+<template>
+  <base-container>
+    <h2>Active Users</h2>
+    <base-search
+      @search="updateSearch"
+      :search-term="enteredSearchTerm"
+    ></base-search>
+    <div>
+      <button @click="sort('asc')" :class="{ selected: sorting === 'asc' }">
+        Sort Ascending
+      </button>
+      <button @click="sort('desc')" :class="{ selected: sorting === 'desc' }">
+        Sort Descending
+      </button>
+    </div>
+    <ul>
+      <user-item
+        v-for="user in displayedUsers"
+        :key="user.id"
+        :user-name="user.fullName"
+        :id="user.id"
+        @list-projects="$emit('list-projects', $event)"
+      ></user-item>
+    </ul>
+  </base-container>
+</template>
+
+<script>
+import { toRefs } from "vue";
+import useSearch from "@/hooks/search.js";
+import UserItem from "./UserItem.vue";
+import useSort from "@/hooks/sort.js";
+
+export default {
+  components: {
+    UserItem,
+  },
+  props: ["users"],
+  emits: ["list-projects"],
+  setup(props) {
+    const { users } = toRefs(props);
+
+    const { enteredSearchTerm, availableItems, updateSearch } = useSearch(
+      users,
+      "fullName"
+    );
+
+    const { displayedUsers, sorting, sort } = useSort(
+      availableItems,
+      "fullName"
+    );
+
+    return {
+      enteredSearchTerm,
+      updateSearch,
+      displayedUsers,
+      sorting,
+      sort,
+    };
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
